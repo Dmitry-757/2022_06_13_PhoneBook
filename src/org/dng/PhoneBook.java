9d30e79c1d21e4624a98897e57abe4c28f23c704
Dmitry-757 <glv11.glv11.glv11@gmail.com>
2022-06-17 15:09:20 +0300

    optimize streams getContactByName

--- src/org/dng/PhoneBook.java
+++ src/org/dng/PhoneBook.java
@@ -6,7 +6,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.HashMap;
 import java.util.List;
 
-@CodeVersion(version="0.1",description="phone book realization",lastModification="2022.06.06")
+@CodeVersion(version = "0.1", description = "phone book realization", lastModification = "2022.06.06")
 public class PhoneBook {
     private static HashMap<Integer, Contact> phoneBookMap = new HashMap<>();
     //private static HashMap<String, Contact> contactBookMap = new HashMap<>();
@@ -23,12 +23,12 @@ public class PhoneBook {
         phoneBookMap.clear();
     }
 
-    public static boolean isNumberPresent(int phoneNumber){
+    public static boolean isNumberPresent(int phoneNumber) {
         return phoneBookMap.containsKey(phoneNumber);
     }
 
     public static void addNewRecord(int phoneNumber, @NotNull Contact contact) throws Exception {
-        if (phoneBookMap.containsKey(phoneNumber)){
+        if (phoneBookMap.containsKey(phoneNumber)) {
             throw new Exception("This number is already present!");
         }
         phoneBookMap.put(phoneNumber, contact);
@@ -38,36 +38,35 @@ public class PhoneBook {
 
     public static Contact getContactByPhoneNumber(int phoneNumber) throws Exception {
         if (!phoneBookMap.containsKey(phoneNumber))
-            throw new Exception("number "+phoneNumber+" is not found!");
+            throw new Exception("number " + phoneNumber + " is not found!");
         return phoneBookMap.get(phoneNumber);
     }
 
-    public static List<Contact> getContactByName(@NotNull String name) throws Exception {
-        return phoneBookMap.entrySet().stream()
-                .filter(es -> name.equals(es.getValue().getName()))
-                .map(v -> v.getValue())
+    public static List<Contact> getContactByName(@NotNull String name) {
+        return phoneBookMap.values().stream()
+                .filter(contact -> name.equals(contact.getName()))
                 .toList();
+
+//                .filter(es -> name.equals(es.getValue().getName()))
+//                .map(Map.Entry::getValue)
+//                .toList();
+
 //                .collect(Collectors.toMap(Map.Entry::getKey,Map.Entry::getValue));
 
-//        if (!contactBookMap.containsKey(name))
-//            throw new Exception("name "+name+" is not found!");
-//        int phoneNumber = contactBookMap.get(name);
-//
-//        return getContactByPhoneNumber(phoneNumber);
     }
 
-    public static void printContactByPhone( int phoneNumber){
+    public static void printContactByPhone(int phoneNumber) {
         Contact someBody = null;
         try {
             someBody = PhoneBook.getContactByPhoneNumber(phoneNumber);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        if (someBody!=null)
-            System.out.println("the phone number "+phoneNumber+" belongs to "+someBody.getName());
+        if (someBody != null)
+            System.out.println("the phone number " + phoneNumber + " belongs to " + someBody.getName());
     }
 
-    public static void printContactByName(String name){
+    public static void printContactByName(String name) {
 
         List<Contact> someBody = null;
         try {
@@ -75,15 +74,18 @@ public class PhoneBook {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        if (someBody.size()!=0){
-            someBody.forEach(v -> System.out.println(v.getContactInfo()));
-            //System.out.println(someBody.getContactInfo());
-        }
-        else
-            System.out.println("Contact with name `"+name+"` was not find");
+        if (someBody != null) {
+            if (someBody.size() != 0) {
+                someBody.forEach(v -> System.out.println(v.getContactInfo()));
+                //System.out.println(someBody.getContactInfo());
+            } else
+                System.out.println("Contact with name `" + name + "` was not find");
+        } else
+            System.out.println("Contact with name `" + name + "` was not find");
+
     }
 
-    public static void printAllBook(){
+    public static void printAllBook() {
         getPhoneBookMap()
                 .values()
                 .stream()
